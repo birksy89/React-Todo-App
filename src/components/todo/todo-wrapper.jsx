8266d702d003faa21943f6c72b2c65b653083cd0
Andrew <andrew@purplecs.com>
2016-11-29 10:55:12 +0000

    Starting To Work On Edit Item - Cycle Through State Array to find object

--- src/components/todo/todo-wrapper.jsx
+++ src/components/todo/todo-wrapper.jsx
@@ -63,8 +63,20 @@ class TodoWrapper extends Component {
 
     }
 
-    editItem(editMe) {
-        console.log(editMe);
+    editItem(editMeID) {
+        console.log(editMeID);
+        var temp = this.state.todos;
+
+        console.log(temp);
+        for(var i=0; i<temp.length; i++){
+          console.log(temp[i].id);
+          if(temp[i].id == editMeID){
+            console.log("Found it!");
+          }
+
+        }
+
+
     }
 
     render() {
@@ -92,10 +104,10 @@ class TodoItem extends Component {
         super(props);
         this.state = {
             editMode: false
-        };
+        }
     }
 
-      componentDidMount() {}
+    componentDidMount() {}
 
     removeTodo() {
         this.props.removeItem(this.props.item.id);
@@ -103,40 +115,45 @@ class TodoItem extends Component {
 
     //Edit Stuff
 
-    editTodo() {
-        this.setState({editMode: true})
+    editMode() {
+        this.setState({editMode: true});
         this.props.editItem(this.props.item.id);
     }
 
-
-
     render() {
 
+        var renderThis;
+
         if (this.state.editMode === false) {
-            return (
-                <ListGroupItem>{this.props.item.value}
 
-                    <Button className="pull-right" bsStyle="danger" bsSize="xsmall" onClick={this.removeTodo.bind(this)}>
-                        <span className="glyphicon glyphicon-trash" aria-hidden="true"></span>
-                    </Button>
-                    <Button className="pull-right" bsStyle="warning" bsSize="xsmall" onClick={this.editTodo.bind(this)}>
-                        <span className="glyphicon glyphicon-pencil" aria-hidden="true"></span>
-                    </Button>
-                </ListGroupItem>
-            )
+            renderThis = <ListGroupItem>{this.props.item.value}
+
+                <Button className="pull-right" bsStyle="danger" bsSize="xsmall" onClick={this.removeTodo.bind(this)}>
+                    <span className="glyphicon glyphicon-trash" aria-hidden="true"></span>
+                </Button>
+                <Button className="pull-right" bsStyle="warning" bsSize="xsmall" onClick={this.editMode.bind(this)}>
+                    <span className="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+                </Button>
+            </ListGroupItem>
+
         } else {
-            return (
 
-                <ListGroupItem>
+            renderThis = <ListGroupItem>
 
-                    <FormControl value={this.props.item.value} type="text" onChange={this.handleEdit.bind(this)}/>
+                <FormControl defaultValue={this.props.item.value} type="text"/>
 
-                    <Button className="pull-right" bsStyle="success" bsSize="xsmall">
-                        <span className="glyphicon glyphicon-ok" aria-hidden="true"></span>
-                    </Button>
-                </ListGroupItem>
-            )
+                <Button className="pull-right" bsStyle="success" bsSize="xsmall">
+                    <span className="glyphicon glyphicon-ok" aria-hidden="true"></span>
+                </Button>
+            </ListGroupItem>
+
         }
+
+        return (
+            <div>
+                {renderThis}
+            </div>
+        )
 
     }
 }
